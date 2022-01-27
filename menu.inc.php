8883168b1aa6e9e22b541627a104a03b4810a95d
dim0nchik1 <98547167+dim0nchik1@users.noreply.github.com>
2022-01-27 20:54:11 +0300

    Add files via upload

---
+++ menu.inc.php
@@ -0,0 +1,9 @@
+            <nav">
+                <ul>
+                    <li><a href="#"> Обо мне </a></li>
+                    <li><a href="#"> Мои фото </a></li>
+                    <li><a href="#"> Мои работы </a></li>
+                    <li><a href="#"> Ссылки </a></li>
+                    <li><a href="#"> Контакты </a></li>
+                </ul>    
+            </nav>
